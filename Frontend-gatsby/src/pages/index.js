a84fcc3d16dbc74537453c30e5b0ba90b2c07076
Cassidy Tillemans <cassidybtillemans@gmail.com>
2021-02-09 10:32:38 -0800

    adds frontend folder back to git repo

---
+++ Frontend-gatsby/src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import '../styles/main.scss';
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Cass Tillemans</h1>
+      <h2>Web Developer</h2>
+      <h3>Hello World</h3>
+      <p>Exploring the world of web development with enthusiasm</p>
+    </main>
+  );
+};
+
+export default IndexPage;
